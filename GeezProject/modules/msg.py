16a639a859211e8d9b8c1b3937cc4e63a7ab8b5c
OnlyMidnight <87945444+OnlyMidnight@users.noreply.github.com>
2021-07-31 05:28:56 +0700

    Update msg.py

--- GeezProject/modules/msg.py
+++ GeezProject/modules/msg.py
@@ -20,7 +20,7 @@ class Messages():
       HELP_MSG = [
         ".",
 f"""
-**Hey 👋 Selamat datang kembali di {PROJECT_NAME}
+**Halloo 👋 Selamat datang kembali di {PROJECT_NAME}
 
 ✣️ {PROJECT_NAME} dapat Memutar Lagu di Voice Chat Group Dengan cara yang Mudah.
 
@@ -31,9 +31,9 @@ f"""
 f"""
 **Pengaturan**
 
-1. Jadikan bot sebagai admin
-2. Mulai obrolan suara / VCG
-3. Ketik `/userbotjoin` dan coba /play <nama lagu>
+1. Jadikan bot musik nya sebagai admin.
+2. Mulailah obrolan suara atau VCG.
+3. Ketik `/userbotjoin` dan coba /play <nama lagu>.
 × Jika Assistant Bot bergabung selamat menikmati musik, 
 × Jika Assistant Bot tidak bergabung Silahkan Tambahkan @{ASSISTANT_NAME} ke grup Anda dan coba lagi
 
